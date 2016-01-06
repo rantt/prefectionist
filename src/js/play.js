0ddb4bc2cbb8eb11757c15ea052c8bfac1fec23b
Dave Canam <david.canam@gmail.com>
2016-01-06 15:34:16 -0400

    shapes are draggable and snap to matching slot

--- src/js/play.js
+++ src/js/play.js
@@ -18,6 +18,7 @@ var aKey;
 var sKey;
 var dKey;
 var score = 0;
+var slots,tiles,shapes;
 
 Game.Play = function(game) {
   this.game = game;
@@ -31,13 +32,16 @@ Game.Play.prototype = {
     // this.shapeSprite = this.game.add.sprite(Game.w/2, Game.h/2, 'shapes');
     // this.shapeSprite.anchor.setTo(0.5, 0.5);
 
-    this.shapes = this.game.add.group();
-    this.slots = this.game.add.group();
+    slots = this.game.add.group();
+    tiles = this.game.add.group();
+    shapes = this.game.add.group();
 
     // console.log(this.shapeSprite.animations.frameTotal);
 
     Shape = function(game, x, y, frame) {
       Phaser.Sprite.call(this, game, x, y, 'shapes', frame);
+      this.initialX = x;
+      this.initialY = y;
       this.anchor.setTo(0.5, 0.5);
       this.tint = 0xff00ff;
     };
@@ -48,9 +52,6 @@ Game.Play.prototype = {
     //Create Shapes
     var shape_count = 9;
     for(var i = 1; i < 9; i++) {
-      var shape_x = Game.w/4+(i*42);
-      var shape_y = 200; 
-      this.shapes.add(new Shape(this.game, shape_x, shape_y, i)); //draw shapes
 
       var slot_x = Game.w/4-100+(i*64);
       var slot_y = Game.h/2; 
@@ -59,16 +60,21 @@ Game.Play.prototype = {
       tile.anchor.setTo(0.5, 0.5);
       tile.tint = 0xdcdcdc;
       tile.alpha = 0.5;
+      tiles.add(tile);
 
-      this.slots.add(new Shape(this.game, slot_x, slot_y, i)); //draw shapes
+      slots.add(new Shape(this.game, slot_x, slot_y, i)); //draw shapes
       // this.game.add.sprite(0+i*64, Game.h/2, 'shapes',0);
 
+      var shape_x = Game.w/4+(i*42);
+      var shape_y = 200; 
+      var shape = new Shape(this.game, shape_x, shape_y, i);
+      shape.inputEnabled = true;
+      shape.input.enableDrag(true);
+      shape.events.onDragStop.add(this.onDragStop, this);
+      // shape.input.enableSnap(42,42,false,true);
+      shapes.add(shape); //draw shapes
+
     }
-
-
-
-
-
 
     // // Music
     // this.music = this.game.add.sound('music');
@@ -87,8 +93,38 @@ Game.Play.prototype = {
     this.twitterButton = this.game.add.button(this.game.world.centerX, this.game.world.centerY + 200,'twitter', this.twitter, this);
     this.twitterButton.anchor.set(0.5);
     this.twitterButton.visible = false;
+    console.log(slots);
   },
+  onDragStop(shape, pointer) {
+    console.log('on drag stop' + slots.length);  
+    var slot;
+    for(var i=0; i < slots.length;i++) {
+      if (slots.children[i].frame === shape.frame){
+        slot = slots.children[i];
+        break;
+      }
+    }
 
+    boundsShape = shape.getBounds();
+    boundsSlot = slot.getBounds();
+    if (Phaser.Rectangle.intersects(boundsShape, boundsSlot)) {
+      //TODO:
+      //*  Snap in Place
+      //*  Score Point
+
+      this.game.add.tween(shape).to({x: slot.initialX, y: slot.initialY}, 50, Phaser.Easing.Linear.Out, true, 0);
+      console.log('score point');
+    }else {
+      this.game.add.tween(shape).to({x: shape.initialX, y: shape.initialY}, 300, Phaser.Easing.Linear.Out, true, 0);
+      // shape.x = shape.initialX; 
+      // shape.y = shape.initialY; 
+    }
+
+    // this.game.physics.arcade.overlap(sprite, slots, function(player, enemy) {
+    //   console.log('overlapping true');  
+    // }, null, this);
+
+  },
   update: function() {
 
     // // Toggle Music
